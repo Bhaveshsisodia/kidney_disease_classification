2bc164e5b8bfcba96a839026c6cb4686fa72d7a0
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2026-04-04 22:11:59 +0530

    Bhavesh

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.kidney_disease_classifier import logger
-
-
-
-logger.info("Starting the Kidney Disease Classifier project...")
+from kidney_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from kidney_disease_classifier import logger
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
